f1df8790d446d203503dacf546d835197303c571
shehbaz <mdshehbaz666666@gmail.com>
2025-11-09 12:08:09 +0530

    remov commet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,3 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { Provider } from 'react-redux';
-// import { store } from './store/store';
-// import CategoryListing from './Pages/CategoryListing';
-// import Layout from "./Layout";
-// import PhotographerProfile from "./Pages/PhotographerProfile";
-
-// function App() {
-//   return (
-//     <Provider store={store}>
-//       <BrowserRouter>
-//         <Routes>
-//           <Route path="/" element={<Layout />}>
-//             <Route path="/photographers" element={<CategoryListing />} />
-//             <Route path="/photographers/:id" element={<PhotographerProfile />} />
-//           </Route>
-//         </Routes>
-//       </BrowserRouter>
-//     </Provider>
-//   );
-// }
-
-// export default App;
-
-
-
 // src/App.jsx
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Provider } from "react-redux";
